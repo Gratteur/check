eb3ae8208323d0b99e1cbfb4acdea120111ec246
Nicolas <nicolasvo95@gmail.com>
2018-06-17 23:28:57 +0200

    put heavy processing into butterfly function

--- run.py
+++ run.py
@@ -12,203 +12,135 @@ app = Flask(__name__)
 def home():
     return redirect(('/g'))
 
+def butterfly():
+
+    dict_rig = {}
+    dict_rig_number = {
+                       1: "201", 
+                       2: "202",
+                       3: "203",}
+    dict_pool_int = {"etn": 1, "graft": 2}
+    list_hashrate = []
+
+    for x, y in dict_rig_number.items():
+        try:
+            response = requests.get("http://ripmundocrit.ddns.net:{0}/API.json".format(y), timeout=4)
+            response_json = json.loads(response.text)
+            uptime = response_json["results"]["avg_time"] * response_json["results"]["shares_good"]
+            dict_rig[x] = dict()
+            dict_rig[x].update({"hashrate": response_json["hashrate"]["total"][1]})
+            dict_rig[x].update({"avg_time": response_json["results"]["avg_time"]})
+            dict_rig[x].update({"uptime": "{0}:{1:0>2}:{2:0>2}".format(int(uptime//3600), int(uptime//60%60), int(uptime%60))})
+            dict_rig[x].update({"pool" : response_json["connection"]["pool"]})
+            dict_rig[x].update({"url": "http://ripmundocrit.ddns.net:{0}/r".format(dict_rig_number[x])})
+
+            for key in dict_pool_int.keys():
+                if key in dict_rig[x]["pool"]:
+                    dict_rig[x].update({"coin": dict_pool_int[key]})
+
+            list_hashrate.append(dict_rig[x]["hashrate"])
+
+        except requests.exceptions.RequestException:
+            dict_rig[x] = dict()
+            list_key = ["avg_time", "hashrate", "uptime", "pool", "url", "coin"]
+            for key in list_key:
+                dict_rig[x].update({key: "rig down."})
+
+    total_hashrate = round(sum(list_hashrate), 1)
+
+    dict_rig1 = {}
+    dict_rig1_number = {
+                       #1: "301",
+                       #2: "302",
+                       }
+    list_hashrate1 = []
+
+    for x, y in dict_rig1_number.items():
+        try:
+            response = requests.get("http://ripmundocrit.ddns.net:{0}/API.json".format(y), timeout=4)
+            response_json = json.loads(response.text)
+            uptime = response_json["results"]["avg_time"] * response_json["results"]["shares_good"]
+            dict_rig1[x] = dict()
+            dict_rig1[x].update({"hashrate": response_json["hashrate"]["total"][1]})
+            dict_rig1[x].update({"avg_time": response_json["results"]["avg_time"]})
+            dict_rig1[x].update({"uptime": "{0}:{1:0>2}:{2:0>2}".format(int(uptime//3600), int(uptime//60%60), int(uptime%60))})
+            dict_rig1[x].update({"pool" : response_json["connection"]["pool"]})
+            dict_rig1[x].update({"url": "http://ripmundocrit.ddns.net:{0}/r".format(dict_rig1_number[x])})
+            list_hashrate1.append(dict_rig1[x]["hashrate"])
+
+        except requests.exceptions.RequestException:
+            dict_rig1[x] = dict()
+            list_key = ["avg_time", "hashrate", "uptime", "pool", "url"]
+            for key in list_key:
+                dict_rig1[x].update({key: "rig down."})
+
+    total_hashrate1 = round(sum(list_hashrate1), 1)
+
+    site= "https://www.cryptunit.com/?order=price3h&?filter=all"
+    hdr = {'User-Agent': 'Mozilla/5.0'}
+    req = Request(site, headers=hdr)
+    response = urlopen(req)
+    htmlparser = etree.HTMLParser()
+    tree = etree.parse(response, htmlparser)
+    list_coin =  ['electroneum', 'graft']
+    dict_coin = {}
+    dict_formula = {} 
+    response = requests.get("http://free.currencyconverterapi.com/api/v5/convert?q=EUR_USD&compact=y", timeout=4)
+    response_json = json.loads(response.text)
+
+    for coin in list_coin:
+        list_values = tree.xpath(f'//div[@class="{coin}"]//ul/li/a/label/text()')
+        dict_coin.update({coin: {
+            "price": float(list_values[0]) / response_json["EUR_USD"]["val"],
+            "hashrate": float(list_values[3])*1000000,
+            "reward": float(list_values[4])
+        }}) 
+        profitability = (total_hashrate/dict_coin[coin]["hashrate"]) * dict_coin[coin]["reward"] * dict_coin[coin]["price"] * 720
+        dict_formula.update({coin: profitability})
+
+    coin_to_mine = max(dict_formula, key=lambda key: dict_formula[key])
+    dict_coin_int = {"electroneum": 1, "graft": 2}
+    coin_to_mine = dict_coin_int[coin_to_mine]
+
+    print("\n... dict_rig here:", dict_rig)
+    print("\n... dict_coin here:", dict_coin)
+    print("\n... dict_formula here:", dict_formula)
+    print("\n... coin_to_mine:", coin_to_mine)
+
+    dict_all = {
+            'dict_rig': dict_rig,
+            'dict_rig1': dict_rig1,
+            'total_hashrate': total_hashrate,
+            'total_hashrate1': total_hashrate,
+            'dict_coin': dict_coin,
+            'coin_to_mine': coin_to_mine,
+            'dict_formula': dict_formula,
+            'dict_rig_number': dict_rig_number
+            }
+    return dict_all
+
 @app.route('/g')
 def gintoki():
 
-    dict_rig = {}
-    dict_rig_number = {
-                       1: "201", 
-                       2: "202",
-                       3: "203",}
-    dict_pool_int = {"etn": 1, "graft": 2}
-    list_hashrate = []
+    dict_all = butterfly()
 
-    for x, y in dict_rig_number.items():
-        try:
-            response = requests.get("http://ripmundocrit.ddns.net:{0}/API.json".format(y), timeout=4)
-            response_json = json.loads(response.text)
-            uptime = response_json["results"]["avg_time"] * response_json["results"]["shares_good"]
-            dict_rig[x] = dict()
-            dict_rig[x].update({"hashrate": response_json["hashrate"]["total"][1]})
-            dict_rig[x].update({"avg_time": response_json["results"]["avg_time"]})
-            dict_rig[x].update({"uptime": "{0}:{1:0>2}:{2:0>2}".format(int(uptime//3600), int(uptime//60%60), int(uptime%60))})
-            dict_rig[x].update({"pool" : response_json["connection"]["pool"]})
-            dict_rig[x].update({"url": "http://ripmundocrit.ddns.net:{0}/r".format(dict_rig_number[x])})
-
-            for key in dict_pool_int.keys():
-                if key in dict_rig[x]["pool"]:
-                    dict_rig[x].update({"coin": dict_pool_int[key]})
-
-            list_hashrate.append(dict_rig[x]["hashrate"])
-
-        except requests.exceptions.RequestException:
-            dict_rig[x] = dict()
-            list_key = ["avg_time", "hashrate", "uptime", "pool", "url", "coin"]
-            for key in list_key:
-                dict_rig[x].update({key: "rig down."})
-
-    total_hashrate = round(sum(list_hashrate), 1)
-
-    dict_rig1 = {}
-    dict_rig1_number = {
-                       #1: "301",
-                       #2: "302",
-                       }
-    list_hashrate1 = []
-
-    for x, y in dict_rig1_number.items():
-        try:
-            response = requests.get("http://ripmundocrit.ddns.net:{0}/API.json".format(y), timeout=4)
-            response_json = json.loads(response.text)
-            uptime = response_json["results"]["avg_time"] * response_json["results"]["shares_good"]
-            dict_rig1[x] = dict()
-            dict_rig1[x].update({"hashrate": response_json["hashrate"]["total"][1]})
-            dict_rig1[x].update({"avg_time": response_json["results"]["avg_time"]})
-            dict_rig1[x].update({"uptime": "{0}:{1:0>2}:{2:0>2}".format(int(uptime//3600), int(uptime//60%60), int(uptime%60))})
-            dict_rig1[x].update({"pool" : response_json["connection"]["pool"]})
-            dict_rig1[x].update({"url": "http://ripmundocrit.ddns.net:{0}/r".format(dict_rig1_number[x])})
-            list_hashrate1.append(dict_rig1[x]["hashrate"])
-
-        except requests.exceptions.RequestException:
-            dict_rig1[x] = dict()
-            list_key = ["avg_time", "hashrate", "uptime", "pool", "url"]
-            for key in list_key:
-                dict_rig1[x].update({key: "rig down."})
-
-    total_hashrate1 = round(sum(list_hashrate1), 1)
-
-    site= "https://www.cryptunit.com/?order=price3h&?filter=all"
-    hdr = {'User-Agent': 'Mozilla/5.0'}
-    req = Request(site, headers=hdr)
-    response = urlopen(req)
-    htmlparser = etree.HTMLParser()
-    tree = etree.parse(response, htmlparser)
-    list_coin =  ['electroneum', 'graft']
-    dict_coin = {}
-    dict_formula = {} 
-    response = requests.get("http://free.currencyconverterapi.com/api/v5/convert?q=EUR_USD&compact=y", timeout=4)
-    response_json = json.loads(response.text)
-
-    for coin in list_coin:
-        list_values = tree.xpath(f'//div[@class="{coin}"]//ul/li/a/label/text()')
-        dict_coin.update({coin: {
-            "price": float(list_values[0]) / response_json["EUR_USD"]["val"],
-            "hashrate": float(list_values[3])*1000000,
-            "reward": float(list_values[4])
-        }}) 
-        profitability = (total_hashrate/dict_coin[coin]["hashrate"]) * dict_coin[coin]["reward"] * dict_coin[coin]["price"] * 720
-        dict_formula.update({coin: profitability})
-
-    coin_to_mine = max(dict_formula, key=lambda key: dict_formula[key])
-    dict_coin_int = {"electroneum": 1, "graft": 2}
-    coin_to_mine = dict_coin_int[coin_to_mine]
-
-    print("\n... dict_rig here:", dict_rig)
-    print("\n... dict_coin here:", dict_coin)
-    print("\n... dict_formula here:", dict_formula)
-    print("\n... coin_to_mine:", coin_to_mine)
-
-    return render_template('gintoki.html', dict_rig=dict_rig, dict_rig1=dict_rig1,
-                            total_hashrate1=total_hashrate1, total_hashrate=total_hashrate, 
-                            dict_coin=dict_coin, coin_to_mine = coin_to_mine, dict_formula=dict_formula)
+    return render_template('gintoki.html', 
+                            dict_rig=dict_all['dict_rig'], 
+                            dict_rig1=dict_all['dict_rig1'],
+                            total_hashrate1=dict_all['total_hashrate1'], 
+                            total_hashrate=dict_all['total_hashrate'], 
+                            dict_coin=dict_all['dict_coin'], 
+                            coin_to_mine = dict_all['coin_to_mine'], 
+                            dict_formula=dict_all['dict_formula'],
+                            dict_rig_number=dict_all['dict_rig_number'])
 
 @app.route('/api', methods=['GET'])
 def get_miner ():
-    
-    dict_rig = {}
-    dict_rig_number = {
-                       1: "201", 
-                       2: "202",
-                       3: "203",}
-    dict_pool_int = {"etn": 1, "graft": 2}
-    list_hashrate = []
 
-    for x, y in dict_rig_number.items():
-        try:
-            response = requests.get("http://ripmundocrit.ddns.net:{0}/API.json".format(y), timeout=4)
-            response_json = json.loads(response.text)
-            uptime = response_json["results"]["avg_time"] * response_json["results"]["shares_good"]
-            dict_rig[x] = dict()
-            dict_rig[x].update({"hashrate": response_json["hashrate"]["total"][1]})
-            dict_rig[x].update({"avg_time": response_json["results"]["avg_time"]})
-            dict_rig[x].update({"uptime": "{0}:{1:0>2}:{2:0>2}".format(int(uptime//3600), int(uptime//60%60), int(uptime%60))})
-            dict_rig[x].update({"pool" : response_json["connection"]["pool"]})
-            dict_rig[x].update({"url": "http://ripmundocrit.ddns.net:{0}/r".format(dict_rig_number[x])})
-
-            for key in dict_pool_int.keys():
-                if key in dict_rig[x]["pool"]:
-                    dict_rig[x].update({"coin": dict_pool_int[key]})
-
-            list_hashrate.append(dict_rig[x]["hashrate"])
-
-        except requests.exceptions.RequestException:
-            dict_rig[x] = dict()
-            list_key = ["avg_time", "hashrate", "uptime", "pool", "url", "coin"]
-            for key in list_key:
-                dict_rig[x].update({key: "rig down."})
-
-    total_hashrate = round(sum(list_hashrate), 1)
-
-    dict_rig1 = {}
-    dict_rig1_number = {
-                       #1: "301",
-                       #2: "302",
-                       }
-    list_hashrate1 = []
-
-    for x, y in dict_rig1_number.items():
-        try:
-            response = requests.get("http://ripmundocrit.ddns.net:{0}/API.json".format(y), timeout=4)
-            response_json = json.loads(response.text)
-            uptime = response_json["results"]["avg_time"] * response_json["results"]["shares_good"]
-            dict_rig1[x] = dict()
-            dict_rig1[x].update({"hashrate": response_json["hashrate"]["total"][1]})
-            dict_rig1[x].update({"avg_time": response_json["results"]["avg_time"]})
-            dict_rig1[x].update({"uptime": "{0}:{1:0>2}:{2:0>2}".format(int(uptime//3600), int(uptime//60%60), int(uptime%60))})
-            dict_rig1[x].update({"pool" : response_json["connection"]["pool"]})
-            dict_rig1[x].update({"url": "http://ripmundocrit.ddns.net:{0}/r".format(dict_rig1_number[x])})
-            list_hashrate1.append(dict_rig1[x]["hashrate"])
-
-        except requests.exceptions.RequestException:
-            dict_rig1[x] = dict()
-            list_key = ["avg_time", "hashrate", "uptime", "pool", "url"]
-            for key in list_key:
-                dict_rig1[x].update({key: "rig down."})
-
-    total_hashrate1 = round(sum(list_hashrate1), 1)
-
-    site= "https://www.cryptunit.com/?order=price3h&?filter=all"
-    hdr = {'User-Agent': 'Mozilla/5.0'}
-    req = Request(site, headers=hdr)
-    response = urlopen(req)
-    htmlparser = etree.HTMLParser()
-    tree = etree.parse(response, htmlparser)
-    list_coin =  ['electroneum', 'graft']
-    dict_coin = {}
-    dict_formula = {} 
-    response = requests.get("http://free.currencyconverterapi.com/api/v5/convert?q=EUR_USD&compact=y", timeout=4)
-    response_json = json.loads(response.text)
-
-    for coin in list_coin:
-        list_values = tree.xpath(f'//div[@class="{coin}"]//ul/li/a/label/text()')
-        dict_coin.update({coin: {
-            "price": float(list_values[0]) / response_json["EUR_USD"]["val"],
-            "hashrate": float(list_values[3])*1000000,
-            "reward": float(list_values[4])
-        }}) 
-        profitability = (total_hashrate/dict_coin[coin]["hashrate"]) * dict_coin[coin]["reward"] * dict_coin[coin]["price"] * 720
-        dict_formula.update({coin: profitability})
-
-    coin_to_mine = max(dict_formula, key=lambda key: dict_formula[key])
-    dict_coin_int = {"electroneum": 1, "graft": 2}
-    coin_to_mine = dict_coin_int[coin_to_mine]
-    
-    print("\n... dict_rig here:", dict_rig)
-    print("\n... dict_coin here:", dict_coin)
-    print("\n... dict_formula here:", dict_formula)
-    print("\n... coin_to_mine:", coin_to_mine)
-
-
+    dict_all = butterfly()
+    dict_rig = dict_all['dict_rig']
+    coin_to_mine = dict_all['coin_to_mine']
+    dict_rig_number = dict_all['dict_rig_number']
     dict_coin = {
             '1' : "Electroneum",
             '2' : "Graft"
